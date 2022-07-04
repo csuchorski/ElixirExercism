ebfea8e502c8c18524e9cfb2e0b91656e6a302ed
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-07-04 19:15:08 +0200

    High school sweethearts exercise finished

---
+++ high-school-sweetheart/lib/high_school_sweetheart.ex
@@ -0,0 +1,33 @@
+defmodule HighSchoolSweetheart do
+  def first_letter(name) do
+    String.trim(name) |> String.first()
+  end
+
+  def initial(name) do
+    first_letter(name) |> String.capitalize() |> String.replace_suffix("", ".")
+  end
+
+  def initials(full_name) do
+    [first_name, last_name] = String.split(full_name)
+    [initial(first_name) <> " ", initial(last_name)] |> List.to_string()
+  end
+
+  def pair(full_name1, full_name2) do
+    """
+         ******       ******
+       **      **   **      **
+     **         ** **         **
+    **            *            **
+    **                         **
+    **     #{initials(full_name1)}  +  #{initials(full_name2)}     **
+     **                       **
+       **                   **
+         **               **
+           **           **
+             **       **
+               **   **
+                 ***
+                  *
+    """
+  end
+end
