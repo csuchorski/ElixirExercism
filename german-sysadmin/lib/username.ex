b1798c1b01dc450bbd53b93ae41480c97d477664
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-07-09 22:42:47 +0200

    german-sysadmin finished

---
+++ german-sysadmin/lib/username.ex
@@ -0,0 +1,39 @@
+defmodule Username do
+  def sanitize(username, built_name \\ [])
+
+  def sanitize('', built_name) do
+    built_name |> Enum.reverse()
+  end
+
+  @spec sanitize(charlist()) :: charlist()
+  def sanitize(username, built_name) do
+    # ä becomes ae
+    # ö becomes oe
+    # ü becomes ue
+    # ß becomes ss
+    [first | tail] = username
+
+    case first do
+      first when first >= ?a and first <= ?z ->
+        sanitize(tail, [first | built_name])
+
+      ?_ ->
+        sanitize(tail, [first | built_name])
+
+      ?ä ->
+        sanitize(tail, [?e, ?a] ++ built_name)
+
+      ?ö ->
+        sanitize(tail, [?e, ?o] ++ built_name)
+
+      ?ü ->
+        sanitize(tail, [?e, ?u] ++ built_name)
+
+      ?ß ->
+        sanitize(tail, [?s, ?s] ++ built_name)
+
+      _ ->
+        sanitize(tail, built_name)
+    end
+  end
+end
