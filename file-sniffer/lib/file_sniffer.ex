6a7add4831651132d62406cabf5a3df153376084
Cezary Suchorski <cezary.suchorski@gmail.com>
2022-08-16 14:08:21 +0200

    Modified file sniffer to use multiple function clauses with pattern matching

--- file-sniffer/lib/file_sniffer.ex
+++ file-sniffer/lib/file_sniffer.ex
@@ -1,42 +1,15 @@
 defmodule FileSniffer do
-  def type_from_extension(extension) do
-    case extension do
-      "exe" -> "application/octet-stream"
-      "bmp" -> "image/bmp"
-      "png" -> "image/png"
-      "jpg" -> "image/jpg"
-      "gif" -> "image/gif"
-    end
-  end
+  def type_from_extension("exe"), do: "application/octet-stream"
+  def type_from_extension("bmp"), do: "image/bmp"
+  def type_from_extension("png"), do: "image/png"
+  def type_from_extension("jpg"), do: "image/jpg"
+  def type_from_extension("gif"), do: "image/gif"
 
-  def type_from_binary(file_binary) do
-    <<signature::binary-size(1), body::binary>> = file_binary
-
-    cond do
-      signature in [<<0x7F>>, <<0x45>>, <<0x4C>>, <<0x46>>] ->
-        "application/octet-stream"
-
-      signature in [<<0x42>>, <<0x4D>>] ->
-        "image/bmp"
-
-      signature in [
-        <<0x89>>,
-        <<0x50>>,
-        <<0x4E>>,
-        <<0x0D>>,
-        <<0x0A>>,
-        <<0x1A>>,
-        <<0x0A>>
-      ] ->
-        "image/png"
-
-      signature in [<<0xFF>>, <<0xD8>>, <<0xFF>>] ->
-        "image/jpg"
-
-      signature in [<<0x47>>, <<0x49>>, <<0x46>>] ->
-        "image/gif"
-    end
-  end
+  def type_from_binary(<<0x7F, 0x45, 0x4C, 0x46, _::binary>>), do: "application/octet-stream"
+  def type_from_binary(<<0x42, 0x4D, _::binary>>), do: "image/bmp"
+  def type_from_binary(<<0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, _::binary>>), do: "image/png"
+  def type_from_binary(<<0xFF, 0xD8, _::binary>>), do: "image/jpg"
+  def type_from_binary(<<0x47, 0x49, 0x46, _::binary>>), do: "image/gif"
 
   def verify(file_binary, extension) do
     type_ext = type_from_extension(extension)
